9191b1977d67f426c8719adbf2c617e0ee222583
bindu7772 <manubpatel4@gmail.com>
2018-01-07 21:30:08 -0500

    back button

--- client/src/components/SelectedRecipe/SelectedRecipe.js
+++ client/src/components/SelectedRecipe/SelectedRecipe.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import "./SelectedRecipe.css";
-const SelectedRecipe = props => (
-  <div className="contimg"  >
-      <p>{props.recipeObj.recipeTitle}</p>
-      <img  className="img-fluid" alt="recipe" src={props.recipeObj.recipeImage}/>
-      <p>{props.recipeObj.healthlabels}</p>
-      <p>{props.recipeObj.dietlabels}</p>
-      <p>{props.recipeObj.calories}</p>
-      <p>{props.recipeObj.recipeLink}</p>
+
+class SelectedRecipe extends Component{
+	constructor(props) {
+    super(props);
+  }
+
+  mainPage = () => {
+  	console.log("Hello Back");
+  	console.log(super.props.state.displayChild);
+  }
+  // state = {
+  //   Selectedrecipes: []
+  // };
+
+  render() {
+    return (
+        <div className="contimg"  >
+      <p>{this.props.recipeObj.recipeTitle}</p>
+      <img  className="img-fluid" alt="recipe" src={this.props.recipeObj.recipeImage}/>
+      <p>{this.props.recipeObj.healthlabels}</p>
+      <p>{this.props.recipeObj.dietlabels}</p>
+      <p>{this.props.recipeObj.calories}</p>
+      <p>{this.props.recipeObj.recipeLink}</p>
+      <button onClick={() => this.mainPage()}>Go Back</button>
  </div>
-);
+    );
+  }
+}
+
+// const SelectedRecipe = props => (
+  
+// );
 
 export default SelectedRecipe;
 
